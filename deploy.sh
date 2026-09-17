08a2938055d6a455b55b3d6994ff09e6154d952d
hex2rgb <robert196@163.com>
2026-09-17 09:42:00 +0800

    fix: 更新
    - modified env
    -  deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-COMPOSE=(docker compose -f docker/compose.my.yml)
+COMPOSE=(docker compose -f docker/compose.my.yml --profile browser --profile downloader)
 
 "${COMPOSE[@]}" down --remove-orphans
 "${COMPOSE[@]}" up -d --build
